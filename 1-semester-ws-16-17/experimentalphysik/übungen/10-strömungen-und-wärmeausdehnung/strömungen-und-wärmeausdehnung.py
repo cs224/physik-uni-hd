82c846c6215bc63f7d7c6dd1d4bd31b894eed837
Christian Schuhegger <Christian.Schuhegger@gmx.de>
2017-02-02 07:52:47 +0100

    strömungen-und-wärmeausdehnung.py

--- 1-semester-ws-16-17/experimentalphysik/übungen/10-strömungen-und-wärmeausdehnung/strömungen-und-wärmeausdehnung.py
+++ 1-semester-ws-16-17/experimentalphysik/übungen/10-strömungen-und-wärmeausdehnung/strömungen-und-wärmeausdehnung.py
@@ -25,6 +25,26 @@ solve([e1, e2, e3], R_3, dict=True)
 r1 = solve(Eq(simplify(e1.rhs / e2.rhs),1), R_3, dict=True)
 r1[0][R_3]
 
+Re = Symbol("Re", positive=True)
+Re_K = Symbol("Re_K", positive=True)
+rho = Symbol("\\rho", positive=True)
+barv = Symbol("\\bar v", positive=True)
+R = Symbol("R", positive=True)
+e4 = Eq(Re,2*R*rho*barv/eta)
+
+dotV = Symbol("\dot V", positive=True)
+e5 = Eq(dotV, pi*R**2*barv)
+solve(e5, barv)[0]
+e4_1 = e4.subs(barv, solve(e5, barv)[0]).subs(Re, Re_K)
+
+dotV_1 = Symbol("\dot V_1", positive=True)
+R_1 = Symbol("R_1", positive=True)
+e6 = Eq(dotV_1, 2*dotV_2)
+
+solve(e4_1.subs(R, R_1).subs(dotV, dotV_1),R_1)[0]
+solve(e4_1.subs(R, R_2).subs(dotV, dotV_2),R_2)[0]
+
+#e4.subs([(R, R_1)])
 
 #solve(e1, R_2, dict=True)
 #solve(e1 / e2, R_3, dict=True)
