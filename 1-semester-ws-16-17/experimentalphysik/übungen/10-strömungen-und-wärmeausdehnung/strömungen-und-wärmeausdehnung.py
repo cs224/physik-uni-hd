1f8455a90a3b8944c660e6c7f526bfb539f626de
Christian Schuhegger <Christian.Schuhegger@gmx.de>
2017-02-03 12:01:53 +0100

    strömungen-und-wärmeausdehnung.py

--- 1-semester-ws-16-17/experimentalphysik/übungen/10-strömungen-und-wärmeausdehnung/strömungen-und-wärmeausdehnung.py
+++ 1-semester-ws-16-17/experimentalphysik/übungen/10-strömungen-und-wärmeausdehnung/strömungen-und-wärmeausdehnung.py
@@ -5,19 +5,22 @@ from sympy.solvers import solve
 
 # V˙2=π8ηR42pB−p0l2
 
+p_A = Symbol("p_A", positive=True)
+l_1 = Symbol("l_1", positive=True)
+
 dotV_2 = Symbol("\dot V_2", positive=True)
 eta = Symbol("\eta", positive=True)
 R_2 = Symbol("R_2", positive=True)
 p_B = Symbol("p_B", positive=True)
 p_0 = Symbol("p_0", positive=True)
 l_2 = Symbol("l_2", positive=True)
-e1 = Eq(dotV_2, (pi/8*eta)*R_2**4*((p_B-p_0)/l_2))
+e1 = Eq(dotV_2, (pi/(8*eta))*R_2**4*((p_B-p_0)/l_2))
 solve(e1, R_2)
 
 dotV_3 = Symbol("\dot V_3", positive=True)
 R_3 = Symbol("R_3", positive=True)
 l_3 = Symbol("l_3", positive=True)
-e2 = Eq(dotV_3, (pi/8*eta)*R_3**4*((p_B-p_0)/l_3))
+e2 = Eq(dotV_3, (pi/(8*eta))*R_3**4*((p_B-p_0)/l_3))
 
 e3 = Eq(dotV_2, dotV_3)
 solve([e1, e2, e3], R_3, dict=True)
@@ -41,8 +44,27 @@ dotV_1 = Symbol("\dot V_1", positive=True)
 R_1 = Symbol("R_1", positive=True)
 e6 = Eq(dotV_1, 2*dotV_2)
 
-solve(e4_1.subs(R, R_1).subs(dotV, dotV_1),R_1)[0]
-solve(e4_1.subs(R, R_2).subs(dotV, dotV_2),R_2)[0]
+
+eR1 = solve(e4_1.subs(R, R_1).subs(dotV, dotV_1),R_1)[0]
+eR2 = solve(e4_1.subs(R, R_2).subs(dotV, dotV_2),R_2)[0]
+eR3 = r1[0][R_3].subs(R_2, eR2)
+
+Delta_p_2 = Symbol("\Delta p_2", positive=True)
+Delta_p_1 = Symbol("\Delta p_1", positive=True)
+e7 = Eq(Delta_p_2, p_B - p_0)
+# e8 = Eq(Delta_p_2, )
+e81 = solve(e1, p_B-p_0)
+e8 = Eq(Delta_p_2, e81[0])
+
+e9 = Eq(Delta_p_1, e8.rhs.subs(l_2, l_1).subs(dotV_2, 2*dotV_1).subs(R_2, R_1))
+
+p_0 = Symbol("p_0", positive=True)
+
+eDelta_p_1 = e9.rhs
+eDelta_p_2 = e8.rhs
+
+ep_A = p_0 + eDelta_p_1 + eDelta_p_2
+factor(ep_A)
 
 #e4.subs([(R, R_1)])
 
